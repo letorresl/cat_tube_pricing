20cf0aa3d793cf9a900c135f8ca9853295d3d94b
Luis Enrique Torres Lopez <luis.e.torreslopez@gmail.com>
2015-07-18 10:05:30 -0500

    (mod ad)ejecucion de fun RMSLE

--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -7,7 +7,7 @@
 
 # In[1]:
 
-from Metadatos import generaPathProyecto
+from Metadatos import generaPathProyecto, RMSLE
 from CargaDatos import retornaSets
 from Preparacion import preparaDf, separacionEntrenaObjetivo
 from Modelo import definirModelo
@@ -17,22 +17,41 @@ from Modelo import definirModelo
 
 # # Ejecucion de rutina
 
-# In[2]:
+# In[ ]:
 
 path_proyecto = generaPathProyecto()
 sets_df = retornaSets(path_proyecto = path_proyecto)
 df = preparaDf(sets_df)
-X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962)
 
 
-# In[3]:
+# In[94]:
+
+X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
+
+
+# In[95]:
 
 modelo = definirModelo()
 
 
-# In[4]:
+# In[96]:
 
 modelo.fit(X = X_train.values, y = y_train.values)
+
+
+# In[97]:
+
+modelo.best_score_
+
+
+# In[98]:
+
+RMSLE(y_train, modelo.predict(X_train))
+
+
+# In[99]:
+
+RMSLE(y_test, modelo.predict(X_test))
 
 
 # In[ ]:
