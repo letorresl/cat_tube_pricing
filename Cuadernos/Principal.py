003de98688f9aee6f298dedf980c57c0075a7f92
Luis Enrique Torres Lopez <luis.e.torreslopez@gmail.com>
2015-07-21 12:10:34 -0500

    (envio)preliminar

--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -13,6 +13,11 @@ from Preparacion import preparaDf, separacionEntrenaObjetivo
 from Modelo import definirModelo
 
 
+# In[42]:
+
+from CargaDatos import generaPath
+
+
 # ### Definicion de Funciones
 
 # # Ejecucion de rutina
@@ -21,57 +26,92 @@ from Modelo import definirModelo
 
 path_proyecto = generaPathProyecto()
 sets_df = retornaSets(path_proyecto = path_proyecto)
-df = preparaDf(sets_df)
 
 
 # In[3]:
 
-X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
+prepDf = preparaDf()
 
 
 # In[4]:
 
-modelo = definirModelo()
-
-
-# In[5]:
-
-modelo.fit(X = X_train.values, y = y_train.values)
+df = prepDf.preparar(sets_df)
 
 
 # In[6]:
 
-modelo.best_score_
+df_envio = prepDf.preparar(sets_df, train_o_envio= 'test')
 
 
-# In[7]:
+# In[30]:
 
-RMSLE(y_train, modelo.predict(X_train))
-
-
-# In[8]:
-
-RMSLE(y_test, modelo.predict(X_test))
+df_envio.head(4)
 
 
-# In[9]:
+# In[10]:
 
-df_envio = preparaDf(sets_df, train_o_envio= 'test')
-
-
-# In[13]:
-
-set(df.columns).difference(set(df_envio.columns))
-
-
-# In[14]:
-
-set(df_envio.columns).difference(set(df.columns))
+X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
 
 
 # In[11]:
 
-df_envio.shape
+modelo = definirModelo()
+
+
+# In[12]:
+
+modelo.fit(X = X_train.values, y = y_train.values)
+
+
+# In[13]:
+
+modelo.best_score_
+
+
+# In[14]:
+
+RMSLE(y_train, modelo.predict(X_train))
+
+
+# In[15]:
+
+RMSLE(y_test, modelo.predict(X_test))
+
+
+# In[17]:
+
+X_envio = prepDf.preparar(sets_df, train_o_envio= 'test')
+
+
+# In[18]:
+
+y_envio = modelo.predict(X_envio.values)
+
+
+# In[31]:
+
+import pandas as pd
+
+
+# In[37]:
+
+y_envio = pd.DataFrame(data= y_envio, index= range(1,y_envio.shape[0] + 1), columns= ['cost'])
+y_envio.index.name = 'id'
+
+
+# In[41]:
+
+path_proyecto
+
+
+# In[44]:
+
+generaPath(path_proyecto, 'y_envio')
+
+
+# In[45]:
+
+y_envio.to_csv(path_or_buf= generaPath(path_proyecto, 'y_envio.csv))
 
 
 # In[ ]:
