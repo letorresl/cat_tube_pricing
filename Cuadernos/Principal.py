b6e2e503daae8fb1057b9c9db34a11bccd496c24
Luis Enrique Torres Lopez <luis.e.torreslopez@gmail.com>
2015-07-21 09:35:04 -0500

    (mod ad)prep envio casi exitoso, pendiente vector
    * La vectorizacion de la preparacion fue casi exitosa,
    sin embargo, los diferentes valores que toman las
    variables nominales para el set de entrenamiento
    son distintos para el set de prueba (envio). Esta
    pendiente corregir eso con la implementacion de una
    clase que almacene la informacion de las clases
    formadas en la preparacion del entrenamiento.

--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -17,41 +17,61 @@ from Modelo import definirModelo
 
 # # Ejecucion de rutina
 
-# In[ ]:
+# In[2]:
 
 path_proyecto = generaPathProyecto()
 sets_df = retornaSets(path_proyecto = path_proyecto)
 df = preparaDf(sets_df)
 
 
-# In[94]:
+# In[3]:
 
 X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
 
 
-# In[95]:
+# In[4]:
 
 modelo = definirModelo()
 
 
-# In[96]:
+# In[5]:
 
 modelo.fit(X = X_train.values, y = y_train.values)
 
 
-# In[97]:
+# In[6]:
 
 modelo.best_score_
 
 
-# In[98]:
+# In[7]:
 
 RMSLE(y_train, modelo.predict(X_train))
 
 
-# In[99]:
+# In[8]:
 
 RMSLE(y_test, modelo.predict(X_test))
+
+
+# In[9]:
+
+df_envio = preparaDf(sets_df, train_o_envio= 'test')
+
+
+# In[13]:
+
+set(df.columns).difference(set(df_envio.columns))
+
+
+# In[14]:
+
+set(df_envio.columns).difference(set(df.columns))
+
+
+# In[11]:
+
+df_envio.shape
 
 
 # In[ ]:
