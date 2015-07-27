683b0e812adecf2e1d2086af392010df55845109
Luis Enrique Torres Lopez <numerodelcaos@gmail.com>
2015-07-27 19:32:54 +0000

    (mod ad)100 decisores en arbol

--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -7,7 +7,7 @@
 
 # In[1]:
 
-from Metadatos import generaPathProyecto, RMSLE
+from Metadatos import generaPathProyecto, RMSLE, logTransf, antilogTransf
 from CargaDatos import retornaSets
 from Preparacion import preparaDf, separacionEntrenaObjetivo
 from Modelo import definirModelo
@@ -32,16 +32,7 @@ import numpy as np
 
 # ### Definicion de funciones
 
-# In[11]:
-
-def logTransf(df):
-    return np.log(df + 1)
-
-def antilogTransf(df):
-    return np.exp(df) - 1
-
-
-# In[37]:
+# In[5]:
 
 def validarModelo():
     dScores = {'best' : -modelo.best_score_,
@@ -56,20 +47,20 @@ def validarModelo():
 
 # ### Entrenamiento
 
-# In[38]:
+# In[6]:
 
 path_proyecto = generaPathProyecto()
 sets_df = retornaSets(path_proyecto = path_proyecto)
 
 
-# In[39]:
+# In[7]:
 
 prepDf = preparaDf()
 df = prepDf.preparar(sets_df)
 X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
 
 
-# In[40]:
+# In[8]:
 
 modelo = definirModelo()
 modelo.fit(X = X_train.values, y = logTransf(y_train).values.reshape(y_train.shape[0],))
@@ -77,83 +68,88 @@ modelo.fit(X = X_train.values, y = logTransf(y_train).values.reshape(y_train.sha
 
 # ### Presentacion de resultados
 
-# In[41]:
+# In[9]:
 
 modelo.best_params_
 
 
-# In[42]:
+# In[10]:
 
 validarModelo()
 
 
 # ### Eliminacion de valores atipicos
 
-# In[9]:
+# In[ ]:
 
 import pandas as pd
 
 
-# In[10]:
+# In[ ]:
 
 vectErrores = (y_train - pd.DataFrame(data= modelo.predict(X_train), index= X_train.index, columns= ['cost']))**2
 
 
-# In[11]:
+# In[ ]:
 
 cuartiles = vectErrores.quantile(q= [0.25, 0.75])
 
 
-# In[12]:
+# In[ ]:
 
 ric = cuartiles.iloc[1, 0] - cuartiles.iloc[0, 0]
 
 
-# In[13]:
+# In[ ]:
 
 cuartiles.iloc[0, 0] - 1.5*ric, cuartiles.iloc[1, 0] + 1.5*ric
 
 
-# In[14]:
+# In[ ]:
 
 indice = vectErrores[vectErrores.cost < cuartiles.iloc[1, 0] + 1.5*ric].index
 
 
-# In[15]:
+# In[ ]:
 
 X_train = X_train.ix[indice,]
 
 
-# In[16]:
+# In[ ]:
 
 y_train = y_train.ix[indice,]
 
 
-# In[36]:
+# In[ ]:
 
 gg.ggplot(gg.aes(x= 'cost'), data= vectErrores) + gg.geom_boxplot()
 
 
 # ### Experimentacion
 
-# In[33]:
+# In[ ]:
 
 a = modelo.best_estimator_.steps[2][1]
 
 
-# In[43]:
+# In[ ]:
 
 impDf = pd.DataFrame(data= zip(X_train.columns, a.feature_importances_), columns= ['nombre', 'importancia'])
 
 
-# In[62]:
+# In[ ]:
 
 gg.ggplot(gg.aes(x= 'nombre', y= 'importancia'), data= impDf[impDf.importancia >= 0.01]) +gg.geom_bar(stat= 'identity') + gg.theme(axis_text_x= gg.element_text(angle= 45))
 
 
 # ### Envio
 
-# In[23]:
+# In[ ]:
+
+get_ipython().magic(u'pdb')
+
+
+# In[ ]:
 
 X_envio = prepDf.preparar(sets_df, train_o_envio= 'test')
 generarEnvio(modelo, X_envio)
