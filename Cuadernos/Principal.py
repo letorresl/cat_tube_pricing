a6745f873f67d003ab2119673ad5180d4a73d216
Luis Enrique Torres Lopez <numerodelcaos@gmail.com>
2015-07-27 20:22:16 +0000

    (mod limp)

--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -78,114 +78,12 @@ modelo.best_params_
 validarModelo()
 
 
-# ### Eliminacion de valores atipicos
-
-# In[ ]:
-
-import pandas as pd
-
-
-# In[ ]:
-
-vectErrores = (y_train - pd.DataFrame(data= modelo.predict(X_train), index= X_train.index, columns= ['cost']))**2
-
-
-# In[ ]:
-
-cuartiles = vectErrores.quantile(q= [0.25, 0.75])
-
-
-# In[ ]:
-
-ric = cuartiles.iloc[1, 0] - cuartiles.iloc[0, 0]
-
-
-# In[ ]:
-
-cuartiles.iloc[0, 0] - 1.5*ric, cuartiles.iloc[1, 0] + 1.5*ric
-
-
-# In[ ]:
-
-indice = vectErrores[vectErrores.cost < cuartiles.iloc[1, 0] + 1.5*ric].index
-
-
-# In[ ]:
-
-X_train = X_train.ix[indice,]
-
-
-# In[ ]:
-
-y_train = y_train.ix[indice,]
-
-
-# In[ ]:
-
-gg.ggplot(gg.aes(x= 'cost'), data= vectErrores) + gg.geom_boxplot()
-
-
 # ### Experimentacion
-
-# In[ ]:
-
-a = modelo.best_estimator_.steps[2][1]
-
-
-# In[ ]:
-
-impDf = pd.DataFrame(data= zip(X_train.columns, a.feature_importances_), columns= ['nombre', 'importancia'])
-
-
-# In[ ]:
-
-gg.ggplot(gg.aes(x= 'nombre', y= 'importancia'), data= impDf[impDf.importancia >= 0.01]) +gg.geom_bar(stat= 'identity') + gg.theme(axis_text_x= gg.element_text(angle= 45))
-
 
 # ### Envio
 
-# In[ ]:
-
-get_ipython().magic(u'pdb')
-
-
-# In[ ]:
+# In[24]:
 
 X_envio = prepDf.preparar(sets_df, train_o_envio= 'test')
 generarEnvio(modelo, X_envio)
-
-
-# In[24]:
-
-import pandas as pd
-
-
-# In[25]:
-
-path_envio = '~/Proyectos/caterpillar_tube_pricing/Bases/Locales/y_envio_20150727_142316.txt'
-
-
-# In[27]:
-
-y_envio = pd.read_csv(path_envio)
-
-
-# In[28]:
-
-y_envio.cost = antilogTransf(y_envio.cost)
-
-
-# In[33]:
-
-path_envio2 = '/home/numerodelcaos_gmail_com/Proyectos/caterpillar_tube_pricing/Bases/Locales/y_envio_20150727_142316_2.txt'
-
-
-# In[35]:
-
-y_envio.to_csv(path_envio2, index = False)
-
-
-# In[25]:
-
-get_ipython().magic(u'pinfo validarModelo')
 
