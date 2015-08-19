eb539492dedae45441fc3b232936db74d40205bf
Luis Enrique Torres Lopez <luis.e.torreslopez@gmail.com>
2015-08-18 21:37:09 -0500

    (mod ad)curva de learning rate

--- Cuadernos/Modelo.py
+++ Cuadernos/Modelo.py
@@ -32,8 +32,8 @@ def definirModelo(num_procesos = -1):
     escalador = StandardScaler()
     #selectorVar = SelectKBest(k = 30)
     decisor = GradientBoostingRegressor(random_state = 1962, n_estimators= 4000,
-                                        learning_rate= 0.005, max_features= 0.2,
-                                        min_samples_leaf= 8, max_depth= 8)
+                                        learning_rate= 0.02, max_features= 0.2,
+                                        min_samples_leaf= 7, max_depth= 8)
     puntuador = RMSLE_score
     modelo = Pipeline(steps = [('imputador', imputador), ('escalador', escalador),
                                 ('decisor', decisor)])
--- Cuadernos/Principal.py
+++ Cuadernos/Principal.py
@@ -60,9 +60,51 @@ df = prepDf.preparar(sets_df)
 X_train, X_test, y_train, y_test = separacionEntrenaObjetivo(df, semilla = 1962, prop_prueba= 0.3)
 
 
-# In[8]:
+# In[ ]:
 
 modelo = definirModelo()
+
+
+# In[ ]:
+
+import matplotlib.pyplot as plt
+#from sklearn.datasets import load_digits
+#from sklearn.svm import SVC
+from sklearn.learning_curve import validation_curve
+from sklearn.metrics import make_scorer
+
+#digits = load_digits()
+X, y = X_train.values, logTransf(y_train).values.reshape(y_train.shape[0],)
+RMSLE_score = make_scorer(RMSLE, greater_is_better=False)
+
+
+param_range = [0.02, 0.01, 0.005]
+train_scores, test_scores = validation_curve(
+    modelo, X, y, param_name="decisor__learning_rate", param_range=param_range,
+    cv=10, scoring=RMSLE_score, n_jobs=4)
+train_scores, test_scores = -train_scores, -test_scores
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+plt.title("Validation Curve with GBR")
+plt.xlabel("$\n_estimators$")
+plt.ylabel("Score")
+plt.ylim(0.0, 0.25)
+plt.semilogx(param_range, train_scores_mean, label="Training score", color="r")
+plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                 train_scores_mean + train_scores_std, alpha=0.2, color="r")
+plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+             color="g")
+plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                 test_scores_mean + test_scores_std, alpha=0.2, color="g")
+plt.legend(loc="best")
+plt.show()
+
+
+# In[8]:
+
 modelo.fit(X = X_train.values, y = logTransf(y_train).values.reshape(y_train.shape[0],))
 
 
